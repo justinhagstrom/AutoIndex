d489866e793983424ff10478cfc09b7952e8af23
SeriousBuggie <70026933+SeriousBuggie@users.noreply.github.com>
2020-08-22 17:19:28 +0300

    Improve speed

--- classes/Item.php
+++ classes/Item.php
@@ -44,67 +44,67 @@ abstract class Item
 	 * @var string
 	 */
 	protected $filename;
-	
+
 	/**
 	 * @var Size
 	 */
 	protected $size;
-	
+
 	/**
 	 * @var int Last modified time
 	 */
 	protected $m_time;
-	
+
 	/**
 	 * @var int Last accessed time
 	 */
 	protected $a_time;
-	
+
 	/**
 	 * @var int
 	 */
 	protected $downloads;
-	
+
 	/**
 	 * @var string
 	 */
 	protected $description;
-	
+
 	/**
 	 * @var string The HTML text of the link to the type icon
 	 */
 	protected $icon;
-	
+
 	/**
 	 * @var string The HTML text of the "[New]" icon
 	 */
 	protected $new_icon;
-	
+
 	/**
 	 * @var string The HTML text of the link to this file or folder
 	 */
 	protected $link;
-	
+
 	/**
 	 * @var string The HTML text of the link to the thumbnail picture
 	 */
 	protected $thumb_link;
-	
+
 	/**
 	 * @var string The HTML text of the link to find the md5sum
 	 */
 	protected $md5_link;
-	
+
 	/**
 	 * @var string The name and path of the parent directory
 	 */
 	protected $parent_dir;
-	
+
 	/**
 	 * @var bool True if this is a link to '../'
 	 */
 	protected $is_parent_dir;
-	
+
 	/**
 	 * @param int $timestamp Time in UNIX timestamp format
 	 * @return string Formatted version of $timestamp
@@ -117,7 +117,7 @@ abstract class Item
 		}
 		return date(DATE_FORMAT, $timestamp);
 	}
-	
+
 	/**
 	 * @return string Date modified (m_time) formatted as a string
 	 * @see Item::format_date()
@@ -126,16 +126,16 @@ abstract class Item
 	{
 		return self::format_date($this -> m_time);
 	}
-	
+
 	/**
 	 * @return string Date last accessed (a_time) formatted as a string
 	 * @see Item::format_date()
 	 */
-	public function format_a_time()
-	{
-		return self::format_date($this -> a_time);
+	public function format_a_time() {
+		if (is_string($this->a_time)) $this->a_time = fileatime($this->a_time);
+		return self::format_date($this->a_time);
 	}
-	
+
 	/**
 	 * Returns everything after the slash, or the original string if there is
 	 * no slash. A slash at the last character of the string is ignored.
@@ -159,18 +159,18 @@ abstract class Item
 		if (substr($path, -1) != '/') $path .= '/';
 		return $path;
 	}
-	
+
 	/**
 	 * @param string $parent_dir
 	 * @param string $filename
 	 */
-	public function __construct($parent_dir, $filename)
+	public function __construct($parent_dir, $filename, $filemtime = null)
 	{
 		$parent_dir = self::make_sure_slash($parent_dir);
 		$full_name = $parent_dir . $filename;
 		$this -> is_parent_dir = false;
-		$this -> m_time = filemtime($full_name);
-		$this -> a_time = fileatime($full_name);
+		$this -> m_time = $filemtime === null ? filemtime($full_name) : $filemtime;
+		$this -> a_time = $full_name; // lazy
 		$this -> icon = $this -> new_icon = $this -> md5_link = $this -> thumb_link = '';
 		global $descriptions;
 		$this -> description = ((DESCRIPTION_FILE && $descriptions -> is_set($full_name)) ? $descriptions -> __get($full_name) : '&nbsp;');
@@ -182,13 +182,13 @@ abstract class Item
 			$age = (time() - $this -> m_time) / 86400;
 			$age_r = round($age, 1);
 			$s = (($age_r == 1) ? '' : 's');
-			
+
 			$this -> new_icon = (($days_new > 0 && $age <= $days_new) ?
 			(ICON_PATH ? ' <img src="' . $config -> __get('icon_path')
 			. 'new.png" alt="' . "$age_r day$s" . ' old" height="14" width="28" />' : ' <span class="autoindex_small" style="color: #FF0000;">[New]</span>') : '');
 		}
 	}
-	
+
 	/**
 	 * @param string $var The key to look for
 	 * @return bool True if $var is set
@@ -197,7 +197,7 @@ abstract class Item
 	{
 		return isset($this -> $var);
 	}
-	
+
 	/**
 	 * @return string The file or folder name
 	 */
@@ -205,7 +205,7 @@ abstract class Item
 	{
 		return $this -> filename;
 	}
-	
+
 	/**
 	 * @return string The file extension of the file or folder name
 	 */
